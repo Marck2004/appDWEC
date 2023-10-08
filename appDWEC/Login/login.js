6e5f52e0bfc758fdda72ef218feea1ad51f2827d
Marck2004 <124670526+Marck2004@users.noreply.github.com>
2023-10-08 21:52:27 +0200

    Add files via upload

---
+++ appDWEC/Login/login.js
@@ -0,0 +1,70 @@
+var listaUsuarios = [];
+window.addEventListener('load',()=>{
+
+});
+
+function inicioSesion(){
+    const formulario = document.getElementById("formulario");
+    formulario.addEventListener(('submit'),function(event){
+
+    event.preventDefault();
+        
+    var usuarios = document.getElementById("usuario").value;
+    var contraseñas = document.getElementById("contraseña").value;
+
+    var bordeusuario = document.getElementById("usuario");
+    var bordecontraseña = document.getElementById("contraseña");
+
+    if(usuarios==="root" && contraseñas==="1234"){
+
+        window.location.href = "aterrizaje.html";
+    }else{
+        alert("Usuario y/o clave erronea");
+        bordeusuario.style.border = "3px solid red";
+        bordecontraseña.style.border = "3px solid red";
+    }
+
+})
+}
+
+    function formularioRegistro(){
+
+    var sectionHiddenRegistro = document.getElementById('UsuarioNuevoRegistro');
+    var ocultarInicioSesion = document.getElementById('inicioSesionOculto');
+    var envioRegistro = document.getElementById('Registro');
+
+    if (sectionHiddenRegistro.style.display == "block") {
+        sectionHiddenRegistro.style.display = "none";
+        ocultarInicioSesion.style.display = "block";
+        envioRegistro.style.display = "none";
+    }else{
+        sectionHiddenRegistro.style.display = "block";
+        ocultarInicioSesion.style.display = "none";
+        envioRegistro.style.display = "block";
+    }
+    
+}
+    function validarRegistroUsuario(){
+        var registraremail = document.getElementById('emailRegistro').value.trim();
+        var registrartlf = document.getElementById('tlfRegistro').value.trim();
+        var registrarUsuario = document.getElementById('usuarioRegistro').value.trim();
+        var registrarContraseña = document.getElementById('contraseñaRegistro').value.trim();
+
+        var nuevoUsuario = new Usuario(registraremail,registrartlf,registrarUsuario,registrarContraseña);
+        
+        if(nuevoUsuario.comprobarEmail(registraremail)){
+
+        }else if(nuevoUsuario.comprobarContraseña(registrarContraseña)){
+
+        }else if(nuevoUsuario.comprobartlf(registrartlf)){
+            
+        }else{
+            listaUsuarios.push(nuevoUsuario);
+            console.log(" email "+registraremail+" tlf "+registrartlf+" Usuario "+registrarUsuario+" contraseña "+registrarContraseña+
+            " Añadido con exito");
+            console.log(listaUsuarios);
+        }
+
+        
+        
+    }
