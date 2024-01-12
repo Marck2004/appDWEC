7ab2fb9b9b215f2a26e18767b1af4a336ea12883
Marck2004 <marcosmaestrocobo@gmail.com>
2024-01-12 14:12:41 +0100

    acabarExamen

--- enunciadoBaile/listaEntrenadores.php
+++ enunciadoBaile/listaEntrenadores.php
@@ -6,7 +6,7 @@ $dato=$_POST["TURNO"];
 
 $resultado=mysqli_query($con,"SELECT DISTINCT curso_cod_entrenador,curso_entrenador FROM cursos_de_baile WHERE CURSO_HORARIO='".$dato."'");
 while ($entrenador=mysqli_fetch_array($resultado)) {
-        echo json_encode($entrenador[0]  . "/" . $entrenador[1] . ";");
+        echo $entrenador[0]  . "/" . $entrenador[1] . ";";
 }
 
 ?>
