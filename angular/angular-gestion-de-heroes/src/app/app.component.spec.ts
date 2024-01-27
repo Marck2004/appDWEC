750c19897bdfaffcce8f0db5e65e237c76cecc12
Marck2004 <marcosmaestrocobo@gmail.com>
2024-01-27 15:22:29 +0100

    ok

--- angular/angular-gestion-de-heroes/src/app/app.component.spec.ts
+++ angular/angular-gestion-de-heroes/src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'angular-gestion-de-heroes' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-gestion-de-heroes');
+    expect(app.titulo).toEqual('angular-gestion-de-heroes');
   });
 
   it('should render title', () => {
