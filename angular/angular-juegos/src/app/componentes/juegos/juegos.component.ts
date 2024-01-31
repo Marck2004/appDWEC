b9be715b367e7ab5504268545105e8aa9cab17e5
Marck2004 <marcosmaestrocobo@gmail.com>
2024-01-31 15:28:50 +0100

    ok

---
+++ angular/angular-juegos/src/app/componentes/juegos/juegos.component.ts
@@ -0,0 +1,29 @@
+import { ServicioJuegoService } from './../../servicios/servicio-juego.service';
+import { Component } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+import { Juego } from '../../interfaces/juego';
+
+@Component({
+  selector: 'app-juegos',
+  standalone: true,
+  imports: [MatTableModule],
+  templateUrl: './juegos.component.html',
+  styleUrl: './juegos.component.css'
+})
+export class JuegosComponent {
+  juegos?:Juego[];
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+
+
+    constructor(private servicioJuegos: ServicioJuegoService){
+      this.juegos=[];
+    }
+    ngOnInit(){
+      this.getJuegos();
+    }
+    getJuegos(){
+      this.servicioJuegos.getJuegos().then(juegosRecibidos =>{
+        this.juegos=juegosRecibidos;
+      })
+    }
+}
---
+++ angular/angular-juegos/src/app/servicios/servicio-juego.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Juego } from '../interfaces/juego';
+const LISTAJUEGOS:Juego[]=[
+  {
+    _id:"1",
+    titulo:"God of war III",
+    descripcion: "Matanza",
+    precio: 45,
+    pegi:18,
+    plataforma:"PS5",
+    foto:"/assets/foto1.png"
+  },
+  {
+    _id:"2",
+    titulo:"Final Fantasy",
+    descripcion: "Creativo",
+    precio: 40,
+    pegi:16,
+    plataforma:"PS5",
+    foto:"/assets/foto2.png"
+  }
+];
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicioJuegoService {
+
+  constructor() { }
+  async getJuegos(): Promise<Juego[]|[]>{
+    return LISTAJUEGOS;
+  }
+}
